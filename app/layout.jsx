d41bb3f4dfe55127a8270096fd9418ad41d2b4d5
Rakib <rakibpro98@gmail.com>
2024-03-03 20:34:04 +0600

    themeToggler added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,12 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+
+// components
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+
+// theme provider
+import { ThemeProvider } from '@/components/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,11 +17,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang='en'>
+        <html lang='en' suppressHydrationWarning>
             <body className={inter.className}>
-                <Header />
-                {children}
-                <Footer />
+                <ThemeProvider attribute='class' defaultTheme='light'>
+                    <Header />
+                    {children}
+                    <Footer />
+                </ThemeProvider>
             </body>
         </html>
     );
